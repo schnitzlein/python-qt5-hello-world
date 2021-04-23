ac3f359cef344fbf2b3d35a63ddca05b76d20308
Juri.Ga <example@gmail.de>
2021-04-23 21:12:55 +0200

    Fixes aka main_window weiterentwickelt

--- main_window.py
+++ main_window.py
@@ -1,5 +1,5 @@
 from PyQt5.QtWidgets import QWidget
-from PyQt5.QtWidgets import QHBoxLayout
+from PyQt5.QtWidgets import QHBoxLayout, QGridLayout, QVBoxLayout
 from PyQt5.QtWidgets import QLabel
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5.QtWidgets import QPushButton
@@ -15,17 +15,17 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("")
         self.screens_config = []
         self.current_screen = 0
-
+        self.button = {}
 
     def say_hello(self):
         print("Button clicked, Hello!")
 
-    def change_widget(self, widget: QWidget, direction: str):
+    def change_widget(self, widget: QWidget, direction: int):
         #print("len: " + str(len(self.screens_config["sub"])))
         max_screen = len(self.screens_config["sub"])
-        if direction == ">":
+        if direction == 0:
             self.current_screen = (self.current_screen + 1) % max_screen
-        elif direction == "<":
+        elif direction == 1:
             self.current_screen = (self.current_screen - 1) % max_screen
         else:
             print("that not a valid direction")
@@ -50,29 +50,47 @@ class MainWindow(QMainWindow):
             #Set Resolution
             window_width = config['main']["resolution"][0]
             window_height = config['main']["resolution"][1]
-            button_width = config['main']["button"][0]
-            button_height = config['main']["button"][1]
+            button_width = config['main']["button-size"][0]
+            button_height = config['main']["button-size"][1]
+            number_of_subs = config['main']["number-of-subs"]
         else:
             title = str(config['name'])
             self.setWindowTitle(title)
             #Set Resolution
             window_width = config["resolution"][0]
             window_height = config["resolution"][1]
-            button_width = config["button"][0]
-            button_height = config["button"][1]
+            button_width = config["button-size"][0]
+            button_height = config["button-size"][1]
+            number_of_subs = config["number-of-subs"]
         self.setFixedSize(window_width, window_height)
 
-        layout = QHBoxLayout()
-        button_left = QPushButton("<")
-        button_left.setFixedSize(button_width * window_width/100, button_height * window_height/100)
-        button_right = QPushButton(">")
-        button_right.setFixedSize(button_width * window_width / 100, button_height * window_height / 100)
+        layout = QGridLayout()
+        vbox = QVBoxLayout()
+
+        hbox_titel = QHBoxLayout()
+        hbox_titel.addStretch()
+        hbox_titel.addWidget(QLabel(title))
+
+        layout.addLayout(hbox_titel, 0, 1)
+        layout.addLayout(vbox, 1, 0)
+
+        button_up = QPushButton("\u1403")
+        button_up.setFixedSize(button_width * window_width/100, button_height * window_height/100)
+        button_down = QPushButton("\u1401")
+        button_down.setFixedSize(button_width * window_width / 100, button_height * window_height / 100)
 
         #self.central_widget = QWidget()
+        vbox.addWidget(button_up)
+
+        for i in range(0, number_of_subs):
+            self.button[i] = QPushButton(self.screens_config["sub"][i]["name"])
+            self.button[i].setFixedSize(button_width * window_width / 100, button_height * window_height / 100)
+            vbox.addWidget(self.button[i])
+
+        vbox.addWidget(button_down)
+
         self.central_widget = Clock()
-        layout.addWidget(button_left)
-        layout.addWidget(self.central_widget)
-        layout.addWidget(button_right)
+        layout.addWidget(self.central_widget, 1, 1)
 
         main_widget = QWidget()
         main_widget.setLayout(layout)
@@ -89,7 +107,7 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(main_widget)
 
         # signals
-        button_left.clicked.connect(lambda: self.change_widget(self.central_widget, "<"))
+        button_up.clicked.connect(lambda: self.change_widget(self.central_widget, 0))
         #print(type(central_widget))
-        button_right.clicked.connect(lambda: self.change_widget(self.central_widget, ">"))
+        button_down.clicked.connect(lambda: self.change_widget(self.central_widget, 1))
 
--- subscreens/clock.py
+++ subscreens/clock.py
@@ -1,8 +1,8 @@
 import sys
-from PyQt5.QtCore import QTime, QTimer
+from PyQt5.QtCore import QTime, QTimer, QDate, Qt
 from PyQt5 import QtWidgets
 from PyQt5.QtGui import QFont
-from PyQt5.QtWidgets import QHBoxLayout
+from PyQt5.QtWidgets import QVBoxLayout, QGridLayout, QHBoxLayout
 from PyQt5.QtWidgets import QApplication, QWidget
 
 class Clock(QWidget):
@@ -10,19 +10,38 @@ class Clock(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.f = QFont("Times", 30, QFont.Bold)
+        self.f = QFont("LCARSGTJ3", 80, QFont.Bold)
+        self.fsmall = QFont("LCARSGTJ3", 40, QFont.Bold)
         self.lblTimeTxt = QtWidgets.QLabel()
         self.lblTimeTxt.setFont(self.f)
+
+        self.lblDateTxt = QtWidgets.QLabel()
+        self.lblDateTxt.setFont(self.fsmall)
+        self.Date = QDate.currentDate().toString("dd.MM.yyyy")
+        self.lblDateTxt.setText(str(self.Date))
 
         self.checkThreadTimer = QTimer(self)
         self.checkThreadTimer.setInterval(100) #.1 seconds
         self.checkThreadTimer.timeout.connect(lambda: self.setTime())
         self.checkThreadTimer.start()
 
-        layout = QHBoxLayout()
-        layout.addStretch(1)
-        layout.addWidget(self.lblTimeTxt)
-        layout.addStretch(1)
+        self.symbolTxt = "\u2026"
+        self.symbol = QtWidgets.QLabel(self.symbolTxt)
+        self.symbol.setFont(self.f)
+
+        self.symbol2 = QtWidgets.QLabel(self.symbolTxt)
+        self.symbol2.setFont(self.f)
+
+        layout = QGridLayout()
+        # hbox = QHBoxLayout()
+        # hbox.addWidget(self.symbol)
+        # hbox.addStretch()
+        # hbox.addWidget(self.symbol2)
+        # layout.addLayout(hbox, 0, 1)
+        layout.addWidget(self.lblTimeTxt, 1, 2, 2, 2)
+        layout.addWidget(self.lblDateTxt, 2, 2, 2, 1)
+
+
         self.setLayout(layout)
         self.show()
 
