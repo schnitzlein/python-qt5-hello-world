1e0d06b0c135f2d989bcd93cf6245042425c48ee
schnitzlein <eracsjob@googlemail.com>
2021-07-12 20:47:14 +0200

    WIP: unfinished but divded into base
    and higher functions

--- util/rest/rest.py
+++ util/rest/rest.py
@@ -3,45 +3,54 @@ import requests
 import json
 
 class Rest():
-    url = None
-    api_key = None
 
-    def __init__(self, url: str, api_key: str):
-        if isinstance(url, str):
-            self.url = url
-        if isinstance(api_key, str):
-            self.api_key = api_key
+    def __init__(self):
+        pass
     
-    def call_server(self, subpath: str) -> dict:
-        print("complete call Path: {}".format(self.url + subpath))
-        resp = requests.get(self.url + subpath + "&appid={}".format(self.api_key))
-        # resp = requests.get(url=url, params=params) # more generic way...
+    def rest_call_get(self, path: str, headers:dict, params: dict) -> dict:
+        payload = {'params': params}   
+        resp = requests.get(path, params=params, headers=headers)
         if resp.status_code == 200:
             try:
                 data = resp.json()    
-                #if data == {}:
-                #    logging.warning("Using old data from callServer()! Code != 200 error is: {}".format(resp.status_code))
-                #    return None
                 ret = { 'code': 200, 'data': data }
                 return ret
             except Exception as e:
-                #logging.error("Something wrong with callServer: {}".format(e))
-                print("Something wrong with callServer: {}".format(e))
+                print("Something went wrong within rest_call_get: {}".format(e))
         elif resp.status_code != 200:
-            print("Calling: '{}' went wrong with code: {}".format(self.url + subpath, resp.status_code))
-            ret = { 'code': resp.status_code, 'data': None }
+            print("Calling: '{}' went wrong with code: {}".format(self.url, resp.status_code))
+            ret = { 'code': resp.status_code, 'data': self.url }
+            return ret
+        else:
+            print("something different happend.")
+    
+    def rest_call_post(self, path: str, headers:dict, params: dict) -> dict:
+        payload = {'params': params}
+        resp = requests.post(path, params=params, headers=headers)
+        if resp.status_code == 200:
+            try:
+                data = resp.json()
+                ret = { 'code': 200, 'data': data }
+                return ret
+            except Exception as e:
+                print("Something went wrong within rest_call_post: {}".format(e))
+        elif resp.status_code != 200:
+            print("Calling: '{}' went wrong with code: {}".format(self.url, resp.status_code))
+            ret = { 'code': resp.status_code, 'data': self.url }
             return ret
         else:
             print("something different happend.")
         
 
 if __name__ == "__main__":
-    r = Rest("http://api.openweathermap.org", "36dcf663b6964439a18574709e1d6eef")
+    r = Rest()
+    key = "36dcf663b6964439a18574709e1d6eef"
     data = None
     city = "Berlin"
+    units = "metric"
     language = "de" # en, fr, ... , https://openweathermap.org/current#multi
-    subpath = "/data/2.5/weather?q={}&units=metric&lang={}".format(city, language)
-    data = r.call_server(subpath=subpath)
+    path = "http://api.openweathermap.org/data/2.5/weather?q={}&units={}&lang={}&appid={}".format(city, units, language,key)
+    data = r.rest_call_get(path=path, headers={}, params={})
     if data['code'] == 200:
        print(data['data'])
     else:
